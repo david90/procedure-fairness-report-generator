b6ece900dad391eecb7e37b3fee021d43dd36b81
david90 <david.ng@oursky.com>
2013-11-07 13:10:54 +0800

    Bug fix on reload link

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 (function() {
-  var ShowDate, getUrlVar, load_from_url, prepare_for_reoprt_and_generate, raw_text_html, raw_title, reload, reloadWithQueryStringVars, report_generate,
+  var ShowDate, getUrlVar, load_from_url, prepare_for_reoprt_and_generate, raw_text_html, raw_title, reloadPage, reloadWithQueryStringVars, report_generate,
     _this = this;
 
   reloadWithQueryStringVars = function(queryStringVars) {
@@ -132,8 +132,8 @@
     });
   };
 
-  reload = function() {
-    return window.location.href = location.href;
+  reloadPage = function() {
+    return window.location.href = window.location.href.split('?')[0];
   };
 
   $('body .generate').on("click", prepare_for_reoprt_and_generate);
